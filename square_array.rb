76449123b9294a4f88ca5455548f2bad9a301e03
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-14 18:52:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  numbers = [4,5,6]
-  square_array(numbers)
-  new_numbers = [6,8,10,12]
-  square_array(new_numbers)
+  numbers = [1,2,3,4] 
+  array.each do |numbers| 
+    square = numbers * numbers 
+    new_arr.push(square)
+ end
+  return new_arr
 end
